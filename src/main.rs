11ef2fbe5e70f98f5fac143bf40d3add1bb22baf
StillScripts <80142739+StillScripts@users.noreply.github.com>
2021-03-13 18:47:52 +1100

    Updated game messages

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,17 @@ use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
-    println!("Guess the number!");
+    println!(" ");
+    println!("I have chosen a number between 1 and 100!");
+    println!("Guess which number I have chosen...");
 
-    let secret_number = rand::thread_rng().gen_range(0..100);
-
-    // println!("The secret number is: {}", secret_number);
+    let secret_number = rand::thread_rng().gen_range(1..100);
+    
+    let mut attempts: u8 = 1;
 
     loop {
-
-        println!("Please input your guess.");
+        println!(" ");
+        println!("Attempt {}: ", &attempts);
 
         let mut guess = String::new();
 
@@ -24,15 +26,21 @@ fn main() {
             Err(_) => continue,
         };
 
-        println!("You guessed: {}", guess);
-
         match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
+            Ordering::Less => {
+                println!("You guessed: {}. That is lower than my number.", guess);
+                attempts = attempts + 1;
+            },
+            Ordering::Greater => {
+                println!("You guessed: {}. That is higher than my number.", guess);
+                attempts = attempts + 1;
+            },
             Ordering::Equal => {
-                println!("You win!");
+                println!("Congratulations, you are correct!");
+                println!("You guessed the number was {} in only {} attempts!", &secret_number, &attempts);
                 break;
             }
         }
     }
 }
+
